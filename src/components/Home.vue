8ccdff17e3db559c3c181fbd930fda161648ca41
kevin uehara <uehara.kevin@gmail.com>
2020-06-19 22:43:50 -0300

    feat(Home): implements Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-form>
+        <v-file-input
+          label="Selecione as Legendas"
+          prepend-icon="mdi-message-text"
+          append-outer-icon="mdi-send"
+          outlined
+          multiple
+          chips
+          v-model="files"
+          @click:append-outer="processSubtitles"
+        />
+      </v-form>
+      <div class="pills">
+        <Pill
+          v-for="word in groupedWords"
+          :key="word.name"
+          :name="word.name"
+          :amount="word.amount"
+        />
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Pill from "./Pill";
+
+export default {
+  components: { Pill },
+  data: function() {
+    return {
+      files: [],
+      groupedWords: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 850 },
+      ],
+    };
+  },
+
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
