3f7b6a0dfde5eab27a774dd7d29ba6b913f5f044
kevin uehara <uehara.kevin@gmail.com>
2020-06-20 00:52:21 -0300

    feat: implements processing of subtitles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,6 +26,7 @@
 </template>
 
 <script>
+import { ipcRenderer } from "electron";
 import Pill from "./Pill";
 
 export default {
@@ -33,17 +34,17 @@ export default {
   data: function() {
     return {
       files: [],
-      groupedWords: [
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 850 },
-      ],
+      groupedWords: [],
     };
   },
 
   methods: {
     processSubtitles() {
-      console.log(this.files);
+      const paths = this.files.map(f => f.path);
+      ipcRenderer.send("process-subtitles", paths);
+      ipcRenderer.on("process-subtitles", (event, res) => {
+        this.groupedWords = res;
+      });
     },
   },
 };
